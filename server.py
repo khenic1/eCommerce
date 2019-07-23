ff60af027bd9cbada197bfa24b271218925692aa
Kelley Henican <kelleyhenican2@gmail.com>
2019-07-23 14:39:09 -0500

    added an image to HTML and boilerplate server

---
+++ server.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, redirect, session, flash
+import re
+from flask_bcrypt import Bcrypt 
+from mysqlconn import connectToMySQL 
+app = Flask(__name__)
+bcrypt = Bcrypt(app)   
+app.secret_key = '194562354'
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
